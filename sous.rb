3cc23441aff6340938e66678e5116cb6095185a4
Sam Salisbury <samsalisbury@gmail.com>
2015-10-01 16:49:21 +0100

    Update sous v0.0.0-alpha.5

--- sous.rb
+++ sous.rb
@@ -1,13 +1,13 @@
 class Sous < Formula
   desc "Sous tool for building and deploying at OpenTable"
   homepage "https://github.com/opentable/sous"
-  url "http://artifactory.otenv.com/artifactory/internal/sous/sous-v0.0.0-alpha.4-darwin-amd64.tar.gz"
-  version "0.0.0"
-  sha256 "4027a4148ec82de52d725108228dc02b03485624a09e524604aee72468e22c53"
+  url "http://artifactory.otenv.com/artifactory/internal/sous/sous-master-darwin-amd64.tar.gz"
+  version "0.0.0-alpha.5"
+  sha256 "fb16436ce626807c93252ee48941c0a529d42178caeb43f15d83036c8197537a"
   def install
-    prefix.install "sous"
+    bin.install("sous")
   end
   test do
-    sous version
+    system("sous", "version")
   end
 end
