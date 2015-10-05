fdbd83d9d8f7d7407e54fbd12f51e1ce8f92fef1
Sam Salisbury <samsalisbury@gmail.com>
2015-10-05 11:46:07 +0100

    Make sous head- and install-from-source only

--- sous.rb
+++ sous.rb
@@ -1,22 +1,8 @@
 class Sous < Formula
   desc "Sous tool for building and deploying at OpenTable"
   homepage "https://github.com/opentable/sous"
-  url "https://github.com/opentable/sous.git",
-		  :tag => "v0.0.3-alpha.3",
-		  :revision => "11504c79dc9d0308e5c29023bac1a414c90e6c76"
   head "https://github.com/opentable/sous.git"
-  version "v0.0.3-alpha.3" 
-  #bottle do
-  #  root_url "http://artifactory.otenv.com/artifactory/internal/sous"
-  #  sha256 "ff14bb2f5d3d3b43423b3ec0daf592193975ce4166b7599b87f5a1f8a392c0ef" => :el_capitan 
-  #  sha256 "ff14bb2f5d3d3b43423b3ec0daf592193975ce4166b7599b87f5a1f8a392c0ef" => :yosemite 
-  #  sha256 "ff14bb2f5d3d3b43423b3ec0daf592193975ce4166b7599b87f5a1f8a392c0ef" => :mavericks 
-  #  sha256 "ff14bb2f5d3d3b43423b3ec0daf592193975ce4166b7599b87f5a1f8a392c0ef" => :mountain_lion 
-  #end
   depends_on "go" => :build
-  #go_resource "github.com/tools/godep" do
-  #  url "https://github.com/tools/godep.git", :revision => "58d90f262c13357d3203e67a33c6f7a9382f9223"
-  #end
   def install
     ENV["GOPATH"] = buildpath
     ENV["PATH"] = ENV["PATH"] + ":" + ENV["GOPATH"] + "/bin"
@@ -25,10 +11,13 @@ class Sous < Formula
     system "mv .git src/github.com/opentable/sous/"
     system "go", "get", "-u", "github.com/tools/godep"
     cd "src/github.com/opentable/sous" do
-      ENV["VERSION"] = "v0.0.3-alpha.3"
-      # This line tries to set BRANCH=tag, if it fails, sets BRANCH=branch name or 'HEAD' if detached
-      # This is used for `sous version`
-      system "BRANCH=$(git describe --exact-match --abbrev=0 --tags 2>/dev/null) || BRANCH=$(git rev-parse --abbrev-ref HEAD); ./scripts/build darwin/amd64; cp artifacts/$BRANCH/darwin/amd64/sous $GOPATH/sous"
+      # These lines try to set BRANCH=tag, if it fails, sets BRANCH=branch name or 'HEAD' if detached
+      set_tag = "BRANCH=$(git describe --exact-match --abbrev=0 --tags 2>/dev/null)"
+      set_branch = "BRANCH=$(git rev-parse --abbrev-ref HEAD)"
+      set_BRANCH = "#{set_tag} || #{set_branch}"
+      compile_sous = "./scripts/build darwin/amd64"
+      copy_artifact = "cp artifacts/$BRANCH/darwin/amd64/sous $GOPATH/sous"
+      system "#{set_BRANCH} && #{compile_sous} && #{copy_artifact}"
       bin.install ENV["GOPATH"] + "/sous"
     end
   end
