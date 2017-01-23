714d6441a5c8bbd415c534526e5942d7f9014979
Judson <nyarly@gmail.com>
2017-01-23 12:09:03 -0800

    Quick sketch of easy-to-update Sous formula
    I'm intrigued by docs in `brew help create` that imply that one can
    write templates *in homebrew* which would let us e.g. `brew create <url
    for most recent tarball>` and be done.

--- sous.rb
+++ sous.rb
@@ -1,6 +1,30 @@
 class Sous < Formula
   desc "Sous tool for building and deploying at OpenTable"
   homepage "https://github.com/opentable/sous"
+
+  # When the version of Sous changes, these two fields need to be updated
+  version "0.1.7"
+  sha256 "539ca446ecd9f931cb6eba24312f7dd1c724c4222140a701ababa3aef9265a1e"
+
+  url "https://github.com/opentable/sous/releases/download/v#{version}/sous-darwin-amd64_#{version}.tar.gz"
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    bin.install "sous"
+  end
+
+  test do
+    system "sous", "version"
+  end
+end
+
+=begin
+
+class Sous < Formula
   head "https://github.com/opentable/sous.git"
   depends_on "go" => :build
   def install
