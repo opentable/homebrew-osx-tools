5e3123a1c4b8ef0ddf0e13cbe3c346f592e32806
Judson <nyarly@gmail.com>
2017-01-24 13:25:57 -0800

    Fixing URL, commenting on horrorshow

--- discover.rb
+++ discover.rb
@@ -4,7 +4,11 @@ class Discover < Formula
   version "0.0.6"
   sha256 "43669bd78f2552e0bf96f796d9ef328537b8a4031c895e617e66caed68b66571"
 
-  url "https://github.com/opentable/discover/archive/v0.0.6.tar.gz"
+  # Github releases to private repos return 404 without an Oauth token, which
+  # would need to be distributed in this (public, because brew...) repo.
+  # Therefore, when discover is released, the source tarball needs to be
+  # deployed as well to artifactory
+  url "http://artifactory.otenv.com/artifactory/dev_tools/discover-0.0.6.tar.gz"
 
   depends_on "go" => :build
 
