78fbfd80dd6d822536bbbe08a3ab379460acac05
Judson <nyarly@gmail.com>
2017-02-02 15:28:09 -0800

    Fixing URL and SHA

--- sous.rb
+++ sous.rb
@@ -4,9 +4,9 @@ class Sous < Formula
 
   # When the version of Sous changes, these two fields need to be updated
   version "0.1.8"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "5eab36f22813350520102298a677eaa6614521b57e594ef5a5d06446e5973f4a"
 
-  url "https://github.com/opentable/sous/releases/download/v#{version}/sous-darwin-amd64_#{version}.tar.gz"
+  url "https://github.com/opentable/sous/releases/download/#{version}/sous-darwin-amd64_#{version}.tar.gz"
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
